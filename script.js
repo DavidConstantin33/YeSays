1e0204b882436566c28b2a327b67e58723bc1546
David <142396942+DavidConstantin33@users.noreply.github.com>
2024-04-27 00:15:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById('btn');
+const container = document.querySelector('.container');
+let quoteEl = null;
+
+const renderQuote = function(data) {
+    const quote = data.quote;
+    const newQuoteElement = document.createElement('div');
+    newQuoteElement.classList.add('quote-parent');
+    newQuoteElement.innerHTML = `
+        <div class="quote">
+            <p>${quote}</p>
+        </div>`;
+
+    if (quoteEl) {
+        container.removeChild(quoteEl); // Remove the old quote element
+    }
+
+    quoteEl = newQuoteElement; // Update quote element reference
+    container.appendChild(quoteEl); // Append the new quote element
+}
+
+btn.addEventListener('click', function() {
+    fetch('https://api.kanye.rest')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            renderQuote(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
